c36cee724b00874d63d91428919df883d90c735f
Agoyi Praise <praisewizzy@gmail.com>
2023-10-05 08:35:01 -0700

    Task alx

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,3 @@
+V#red_header').click(function () {
+  $('header').css('color', '#FF0000');
+});
---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+dy(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    data.results.forEach(function (film) {
+      $('<LI>').text(film.title).appendTo('UL#list_movies');
+    });
+  });
+});
